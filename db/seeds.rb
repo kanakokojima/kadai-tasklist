69e2202b4fcbc0ebffd5cf546cf5f8414c1d1dae
jiro.kirameki <marimo0722@gmail.com>
2018-02-27 14:28:55 +0000

    bootstrap design of tasklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "test title1", content: "test content 1")
+Task.create(title: "test title1", content: "test content 2")
+Task.create(title: "test title1", content: "test content 3")
