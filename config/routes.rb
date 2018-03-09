6baa4986b33e7104f045cd638f03acc224393fb7
jiro.kirameki <marimo0722@gmail.com>
2018-03-09 14:41:15 +0000

    tasklist user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: "tasks#index"
   
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
